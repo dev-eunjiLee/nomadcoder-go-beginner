49163a57a212a9ea7e441c1baf47b6a08cc31fa4
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-13 22:32:59 +0900

    [1.4] Function part Two - naked return

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("nico", "lynn", "ethan")
+	totalLength, uppercase := lenAndUpper("nico")
+	fmt.Println(totalLength, uppercase)
 }
