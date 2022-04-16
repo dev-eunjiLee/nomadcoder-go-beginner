025a89576f7d80d193aac57d94d371278bc69199
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 02:17:00 +0900

    [1.11] Structs

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
 
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	// * for문을 이용해 map 열거
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	favFood := []string{"pizza", "pasta"}
+	nico := person{"nico", 18, favFood} // struct 지정하는 방법 1: 순서대로 입력(코드상에서 좋지 못함)
 	fmt.Println(nico)
+	// struct 지정하는 방법 2: key: value 쌍으로 순서대로 입력(코드에서 명확하게 드러나서 좋다)
+	// * 1, 2를 섞어서는 표현 불가
+	ethan := person{
+		name:    "ethan",
+		age:     28,
+		favFood: favFood,
+	}
+
+	v := reflect.ValueOf(ethan)
+	test := v.Type()
+
+	for i := 0; i < test.NumField(); i++ {
+		fmt.Println(i, test.Field(i).Name, v.Field(i))
+	}
+
+	fmt.Println(v, test)
 }
