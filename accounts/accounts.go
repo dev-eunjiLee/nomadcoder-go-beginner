badd79b1171ea47773fc831a0760cf9072103b1c
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 13:15:58 +0900

    [2.0] 생성자 패턴 필드 접근 테스트

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -2,12 +2,12 @@ package accounts
 
 // Account struct
 type Account struct {
-	owner   string
+	Owner   string
 	balance int
 }
 
 // NewAccount : creates Account => struct instance를 만든 후 포인터를 리턴
 func NewAccount(owner string) *Account {
-	account := Account{owner: owner, balance: 0}
+	account := Account{Owner: owner, balance: 0}
 	return &account
 }
