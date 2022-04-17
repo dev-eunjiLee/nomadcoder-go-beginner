1983e3e81aa89aa6d51fd92d78b0cb28bac9d134
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 23:28:53 +0900

    [3.0] hitURL

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
+var errRequestFailed = errors.New("request Fail")
+
+func main() {
+	urls := []string{
+		"https://www.naver.com/",
+		"https://nomadcoders.co/",
+		"https://github.com/",
+	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
