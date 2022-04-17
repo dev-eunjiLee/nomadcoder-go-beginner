7f78d8c5cf563e792c4a707f92961c086740202d
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 21:48:54 +0900

    [2.4] Dictionary part One 파일 정리

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/dev-eunjiLee/learngo/accounts"
-)
-
 func main() {
-	account := accounts.NewAccount("nico") // account struct를 nico를 이용해 instance로 만든 후, 해당 인스턴스의 주소값을 리턴
-	account.Deposit(3)
-	fmt.Println(account)
+
 }
