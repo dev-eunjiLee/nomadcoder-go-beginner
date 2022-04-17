5d20552c562bd06bc936149fcf3d977f19e5cc04
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 13:39:13 +0900

    [2.1] Methods part One 1.19

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,20 @@
 package accounts
 
+import "fmt"
+
 // Account struct
 type Account struct {
-	Owner   string
+	owner   string
 	balance int
 }
 
 // NewAccount : creates Account => struct instance를 만든 후 포인터를 리턴
 func NewAccount(owner string) *Account {
-	account := Account{Owner: owner, balance: 0}
+	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit : (a Account)라고 선언해줌으로써 Account struct에 Deposit 메소드가 선언되었다
+func (a Account) Deposit(amount int) {
+	fmt.Println(amount)
 }
--- main.go
+++ main.go
@@ -7,17 +7,6 @@ import (
 
 func main() {
 	account := accounts.NewAccount("nico") // account struct를 nico를 이용해 instance로 만든 후, 해당 인스턴스의 주소값을 리턴
-
-	// 포인터를 역참조해서 필드에 접근할 수는 없지만, 역참조한 인스턴스를 다른 변수에 할당해 해당 인스턴스를 수정하는 방식은 가능하다.
-	newAccount := *account
-	newAccount.Owner = "nico2"
-
-	fmt.Println(&newAccount, account) // newAccount
-	fmt.Println(newAccount)           // {nico2 0}
-
-	test := accounts.Account{Owner: "test"}
-	test.Owner = "test2"
-	fmt.Println(test)
-
+	account.Deposit(3)
 	fmt.Println(account)
 }
