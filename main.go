3192c17c754059d180cb49a0e1a7a8e304b30f94
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-17 23:15:40 +0900

    [3.0] hitURL 파일 및 코드 정리

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/dev-eunjiLee/learngo/mydict"
-)
-
 func main() {
-
-	dictionary := mydict.Dictionary{}
-
-	baseWord := "hello"
-
-	dictionary.Add(baseWord, "First")
-
-	err := dictionary.Update(baseWord, "Second")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
-
-	deleteWord := "hello"
-	dictionary.Delete(deleteWord)
-	word2, err2 := dictionary.Search(deleteWord)
-
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-
-	fmt.Println("after Delete ", word2)
 
 }
