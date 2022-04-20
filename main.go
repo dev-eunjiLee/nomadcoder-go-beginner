bd49dc2b3625f9a9ee6d2754511e7bf93dfa1741
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-20 23:25:44 +0900

    [3.2] Goroutines

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 2)
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second) // * 1초 슬립
+	}
 }
