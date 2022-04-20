02180006763afd28b196577e4f1cd2cf66059a4a
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-20 23:53:00 +0900

    [3.3] Channels

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	go sexyCount("flynn")
-	time.Sleep(time.Second * 2)
+	c := make(chan string)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	//result := <-c // 메인에서 채널의 결과를 기다리고 있으면, 메인 함수가 끝나지 않는다.
+	//fmt.Println(result)
+
+	fmt.Println(<-c)
+
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second) // * 1초 슬립
-	}
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 1)
+	c <- person
 }
