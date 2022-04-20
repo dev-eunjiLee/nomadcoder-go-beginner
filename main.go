0a65e4e75cf9e5596ce1f9dd7dcff03d68dd6683
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-20 23:13:05 +0900

    [3.2] 하기전 코드 정리

--- main.go
+++ main.go
@@ -1,46 +1,5 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-)
-
-var errRequestFailed = errors.New("request Fail")
-
 func main() {
 
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.naver.com/",
-		"https://nomadcoders.co/",
-		"https://github.com/",
-		"https://www.reddit.com/",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAIL"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
-
-}
-
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		fmt.Println("test")
-		return errRequestFailed
-	}
-	return nil
 }
