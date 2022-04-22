fa99721b4d12cf28e93fb81dac7a1e8cdcef4662
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-22 23:52:43 +0900

    [4.0] getPages part One

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+var baseUrl string = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&vjk=6dd48f3771d01215"
+
+func main() {
+	getPages()
+}
+
+// getPages: 페이지 수 리턴하는 함수
+func getPages() {
+	res, err := http.Get(baseUrl)
+
+	// 에러가 난 경우 프로그램 종료
+	checkErr(err)
+
+	// statusCode도 체크 후 이상이 있는 경우 프로그램 종료
+	checkCode(res)
+
+	defer res.Body.Close() // res.Body는 IO(입력과 출력)으로 함수가 끝났을 때 닫아야 한다
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination ").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(i, s.Text())
+	})
+
+	fmt.Println(doc)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
+}
